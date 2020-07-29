174e81c11c2468dd028df2b872ac7ed963ad0dc8
Nova <quinton.xu@gmail.com>
2020-07-29 14:45:09 +0800

    feat: bullmq worker work with IPC

--- agent.js
+++ agent.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const createQueue = require('./lib/bullmq');
-// const { Worker } = require('bullmq');
+const { Worker } = require('bullmq');
 
 module.exports = agent => {
   agent.logger.info('[egg-bullmq] plugin init');
@@ -14,28 +14,47 @@ module.exports = agent => {
   class QueueStrategy extends agent.ScheduleStrategy {
     start() {
       const { schedule } = this;
-      const { default: { redis } } = config;
-      if (schedule.worker === 'all') {
-        this.sendAll(Object.assign({}, schedule, { redis }));
-      } else if (schedule.worker === 'one') {
-        this.sendOne(Object.assign({}, schedule, { redis }));
-      } else {
-        throw new Error(`[egg-bullmq] unknow worker type ${schedule.worker}`);
-      }
+      const { schedule: { queue: queueName, prefix } } = this;
+      const { default: { redis: connection } } = config;
+      const worker = new Worker(queueName, async job => {
+        agent.coreLogger.info('[egg-bullmq] using egg-bullmq plugin to connect RabbitMQ append to an issue');
+        if (job.data.length > 0) {
+          agent.coreLogger.info('[egg-bullmq] there is non job data passed, default pass');
+          return;
+        }
+        if (schedule.worker === 'all') {
+          this.sendAll({ schedule, job });
+        } else if (schedule.worker === 'one') {
+          this.sendOne({ schedule, job });
+        } else {
+          throw new Error(`[egg-bullmq] unknow worker type ${schedule.worker}`);
+        }
+      }, { connection, prefix });
+      agent.coreLogger.info(`[egg-bullmq] Worker named: ${worker.name} has worke`);
     }
   }
 
   agent.schedule.use('queue', QueueStrategy);
 
   class QueueWorkFlowStrategy extends agent.ScheduleStrategy {
-    start() {
+    async start() {
       const { schedule } = this;
-      const { default: { redis } } = config;
-      if (schedule.worker === 'one') {
-        this.sendOne(Object.assign({}, schedule, { redis }));
-      } else {
-        throw new Error('[egg-bullmq] workflow pattern only support `one`');
-      }
+      const { schedule: { queue: queueName, prefix } } = this;
+      const { default: { redis: connection } } = config;
+      const worker = new Worker(queueName, async job => {
+        agent.coreLogger.info('[egg-bullmq] using egg-bullmq plugin to connect RabbitMQ append to an issue');
+        if (job.data.length > 0) {
+          agent.coreLogger.info('[egg-bullmq] there is non job data passed, default pass');
+          return;
+        }
+        if (schedule.worker === 'one') {
+          this.sendOne({ schedule, job });
+        } else {
+          throw new Error('[egg-bullmq] workflow pattern only support `one`');
+        }
+      }, { connection, prefix });
+      agent.coreLogger.info(`[egg-bullmq] Worker named: ${worker.name} has worke`);
+      // agent.messenger.on('bullmq_ack', data => {});
     }
   }
 
